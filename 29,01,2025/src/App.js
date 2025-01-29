b90a7fab6239a193e1954d001a2a36f5f96d9777
aangesi <angelinabarseva3@gmail.com>
2025-01-29 12:35:04 +0500

    Add files via upload

---
+++ 29,01,2025/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./todo/TodoList";
+
+function App() {
+  return (
+    <div>
+      <h1>Мое ToDo-приложение</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
